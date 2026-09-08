83668e4426c60c99dc2dbe4b4a4360269d76611f
poce poce <samithportilla-cyber>
2026-09-08 15:48:13 -0500

    test reemplaza una subcadena por otra - Workshop.java

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -265,12 +265,11 @@ public int contarPalabras(String cadena) {
         return cadena.toLowerCase();
     }
 
-    // Método que reemplaza una subcadena en una cadena por otra subcadena
-    public String reemplazarSubcadena(String cadena, String antiguaSubcadena, String nuevaSubcadena) {
-        // TODO: Implementar el método para reemplazar una subcadena en una cadena por otra subcadena.
-        // Ejemplo: Si cadena = "Hello Java", antiguaSubcadena = "Java", y nuevaSubcadena = "world", el resultado debería ser "Hello world".
-        return "";
-    }
+    // Método que reemplaza una subcadena por otra
+public String reemplazarSubcadena(String cadena,String antiguaSubcadena,String nuevaSubcadena) {
+    return cadena.replace(antiguaSubcadena, nuevaSubcadena);
+}
+
 
     // Método que busca una subcadena en una cadena y retorna su índice
     public int buscarSubcadena(String cadena, String subcadena) {
