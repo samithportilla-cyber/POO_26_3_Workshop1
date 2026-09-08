6221ef64aaa19a171aaa01699d96aa5e85789103
poce poce <samithportilla-cyber>
2026-09-08 08:46:33 -0500

    Método que combina dos arreglos en uno solo - Workshop.java

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -182,11 +182,16 @@ public boolean buscarElemento(int[] arreglo, int elemento) {
 }
 
     // Método que combina dos arreglos en uno solo
-    public int[] combinarArreglos(int[] arreglo1, int[] arreglo2) {
-        // TODO: Implementar el método para combinar dos arreglos en uno solo.
-        // Ejemplo: Si arreglo1 = [1, 2, 3, 4, 5] y arreglo2 = [6, 7, 8], el resultado debería ser [1, 2, 3, 4, 5, 6, 7, 8].
-        return new int[0];
+   public int[] combinarArreglos(int[] arreglo1, int[] arreglo2) {
+    int[] resultado = new int[arreglo1.length + arreglo2.length];
+    for (int i = 0; i < arreglo1.length; i++) {
+        resultado[i] = arreglo1[i];
     }
+    for (int i = 0; i < arreglo2.length; i++) {
+        resultado[arreglo1.length + i] = arreglo2[i];
+    }
+    return resultado;
+}
 
     // Método que rota un arreglo n posiciones
     public int[] rotarArreglo(int[] arreglo, int posiciones) {
