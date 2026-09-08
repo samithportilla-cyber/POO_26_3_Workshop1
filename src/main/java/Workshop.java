20ed182362a200da4461b01f734b76ed758e08ab
poce poce <samithportilla-cyber>
2026-09-07 22:36:59 -0500

    metodo que invierte un arreglo - Workshop.java

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -135,10 +135,12 @@ public boolean buscarElemento(int[] arreglo, int elemento) {
 
     // Método que invierte un arreglo
     public int[] invertirArreglo(int[] arreglo) {
-        // TODO: Implementar el método para invertir un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser [5, 4, 3, 2, 1].
-        return new int[0];
+    int[] invertido = new int[arreglo.length];
+    for (int i = 0; i < arreglo.length; i++) {
+        invertido[i] = arreglo[arreglo.length - 1 - i];
     }
+    return invertido;
+}
 
     // Método que ordena un arreglo en orden ascendente
     public int[] ordenarArreglo(int[] arreglo) {
