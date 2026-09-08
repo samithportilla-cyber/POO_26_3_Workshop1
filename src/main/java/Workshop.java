d441115aa550392d2a76615d4159259b496265b8
poce poce <samithportilla-cyber>
2026-09-07 21:57:34 -0500

    Actualizacion de Workshop.java

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -99,24 +99,28 @@ public int sumaElementos(int[] arreglo) {
 
 
     // Método que calcula el promedio de los elementos de un arreglo
-    public double promedioElementos(int[] arreglo) {
-        // TODO: Implementar el método para calcular el promedio de los elementos de un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 3.0.
-        return 0.0;
-    }
+public double promedioElementos(int[] arreglo) {
+    int suma = sumaElementos(arreglo);
+    return (double) suma / arreglo.length;
+}
 
     // Método que encuentra el elemento mayor en un arreglo
-    public int encontrarElementoMayor(int[] arreglo) {
-        // TODO: Implementar el método para encontrar el elemento mayor en un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 5.
-        return 0;
+public int encontrarElementoMayor(int[] arreglo) {
+    int mayor = arreglo[0];
+    for (int i = 1; i < arreglo.length; i++) {
+        if (arreglo[i] > mayor) {
+            mayor = arreglo[i];
+        }
     }
-
+    return mayor;
+}
     // Método que encuentra el elemento menor en un arreglo
     public int encontrarElementoMenor(int[] arreglo) {
-        // TODO: Implementar el método para encontrar el elemento menor en un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 1.
-        return 0;
+       int menor = arreglo [0];
+	   for (int i=1; i > arreglo.length; i++){
+		   if (arreglo[i] < menor) {
+			   menor = arreglo [i];
+        return menor;
     }
 
     // Método que busca un elemento en un arreglo
