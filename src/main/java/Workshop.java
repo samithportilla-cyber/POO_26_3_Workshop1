d80f2340875e90f407e1c08bcaac988a2693704b
poce poce <samithportilla-cyber>
2026-09-08 08:29:36 -0500

    metodo que arregla un arreglo en orden ascendente - Workshop.java

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -143,11 +143,19 @@ public boolean buscarElemento(int[] arreglo, int elemento) {
 }
 
     // Método que ordena un arreglo en orden ascendente
-    public int[] ordenarArreglo(int[] arreglo) {
-        // TODO: Implementar el método para ordenar un arreglo en orden ascendente.
-        // Ejemplo: Si arreglo = [5, 4, 3, 2, 1], el resultado debería ser [1, 2, 3, 4, 5].
-        return new int[0];
+   public int[] ordenarArreglo(int[] arreglo) {
+    int[] ordenado = arreglo.clone();
+    for (int i = 0; i < ordenado.length - 1; i++) {
+        for (int j = 0; j < ordenado.length - 1 - i; j++) {
+            if (ordenado[j] > ordenado[j + 1]) {
+                int temporal = ordenado[j];
+                ordenado[j] = ordenado[j + 1];
+                ordenado[j + 1] = temporal;
+            }
+        }
     }
+    return ordenado;
+}
 
     // Método que elimina los duplicados de un arreglo
     public int[] eliminarDuplicados(int[] arreglo) {
